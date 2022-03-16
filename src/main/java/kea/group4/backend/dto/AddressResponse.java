f144ba0d06ddab8b5662b512c40ced0f1453d16a
Christian Hundahl <chri02jx@stud.kea.dk>
2022-03-16 11:19:03 +0100

    Finished AddressRespone, AddressRequest

--- src/main/java/kea/group4/backend/dto/AddressResponse.java
+++ src/main/java/kea/group4/backend/dto/AddressResponse.java
@@ -1,9 +1,13 @@
 package kea.group4.backend.dto;
 
 import kea.group4.backend.entities.Address;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -14,7 +18,14 @@ public class AddressResponse {
     private int zipCode;
     private String city;
 
-    public addressResponse(Address address) {
-        this.id = address.getId();
+    public AddressResponse(Address address) {
+        this.street = address.getStreet();
+        this.additionalInfo = address.getAdditionalInfo();
+        this.zipCode = address.getZipCode();
+        this.city = address.getCity();
+    }
+
+    public static List<AddressResponse> getAddressesFromEntities(List<Address> addresses) {
+        return addresses.stream().map(AddressResponse::new).collect(Collectors.toList());
     }
 }
