34a7b5e09176d3c9e24ee8fb6e4980d9ed4f2079
SirMeows <59692332+SirMeows@users.noreply.github.com>
2022-03-18 16:57:49 +0100

    changed test to use ResultActions

--- src/test/java/kea/group4/backend/api/AddressControllerTest.java
+++ src/test/java/kea/group4/backend/api/AddressControllerTest.java
@@ -3,6 +3,7 @@ package kea.group4.backend.api;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kea.group4.backend.entities.Address;
 import kea.group4.backend.repositories.AddressRepository;
+import org.jboss.jandex.Result;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +12,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -45,14 +46,15 @@ class AddressControllerTest {
 
     @Test
     void getAddressesById() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
-                .get("/api/addresses/" + addressOneId)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.id").exists())
-                .andExpect(jsonPath("$.id").value(addressOneId))
-                .andExpect(jsonPath(("$.street")).value("streetname1"));
+        ResultActions result = mockMvc.perform(MockMvcRequestBuilders
+            .get("/api/addresses/" + addressOneId)
+            .accept(MediaType.APPLICATION_JSON));
+        
+        result.andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.id").exists())
+            .andExpect(jsonPath("$.id").value(addressOneId))
+            .andExpect(jsonPath(("$.street")).value("streetname1"));
     }
 
     @Test
@@ -60,34 +62,36 @@ class AddressControllerTest {
         // https://support.smartbear.com/alertsite/docs/monitors/api/endpoint/jsonpath.html#:~:text=%5B%3F(expression)%5D,node%20being%20processed.
         String zip = "$[?(@.zipCode == %d)]";
         String street = "$[?(@.street == '%s')]";  // for each element in this array, zip equals placeholder string
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/api/addresses/")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0]").exists())
-                .andExpect(jsonPath("$.length()").value(2))
-                .andExpect(jsonPath(zip, 2300).exists())
-                .andExpect(jsonPath(street, "streetname2").exists());
+
+        ResultActions result = mockMvc.perform(MockMvcRequestBuilders
+            .get("/api/addresses/")
+            .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$[0]").exists())
+            .andExpect(jsonPath("$.length()").value(2))
+            .andExpect(jsonPath(zip, 2300).exists())
+            .andExpect(jsonPath(street, "streetname2").exists());
     }
 
     @Test
     void addAddress() throws Exception {
         Address address = new Address("street3", "blablainfo", 1411, "KBH K");
-        mockMvc.perform(MockMvcRequestBuilders
-                    .post("/api/addresses/")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(address)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").exists())
-                .andExpect(jsonPath("$.id", notNullValue()));
+
+        ResultActions result = mockMvc.perform(MockMvcRequestBuilders
+            .post("/api/addresses/")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(address)));
+
+        result.andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        result.andExpect(jsonPath("$.id").exists())
+            .andExpect(jsonPath("$.id", notNullValue()));
 
         assertEquals(3, addressRepository.count());
-
-
-
-
     }
 
     @Test
