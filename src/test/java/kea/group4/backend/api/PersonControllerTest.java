18fe88691472a5a6ccedeca6920a5bdfe5ed2f5c
ptsand <ptrsand@gmail.com>
2022-03-28 09:10:01 +0200

    PersonController integration test

--- src/test/java/kea/group4/backend/api/PersonControllerTest.java
+++ src/test/java/kea/group4/backend/api/PersonControllerTest.java
@@ -1,7 +1,12 @@
 package kea.group4.backend.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kea.group4.backend.dto.HobbyInfoRequest;
+import kea.group4.backend.dto.PersonRequest;
+import kea.group4.backend.entities.Hobby;
 import kea.group4.backend.entities.Person;
+import kea.group4.backend.error.Client4xxException;
 import kea.group4.backend.repositories.HobbyInfoRepository;
 import kea.group4.backend.repositories.PersonRepository;
 import kea.group4.backend.services.PersonService;
@@ -12,8 +17,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -22,23 +30,30 @@ class PersonControllerTest {
 
     @Autowired
     MockMvc mockMvc;
-
     @Autowired
     PersonRepository personRepository;
-
     @Autowired
     HobbyInfoRepository hobbyInfoRepository;
-
     @Autowired
     PersonService personService;
+    @Autowired
+    ObjectMapper objectMapper;
 
     @BeforeEach
     public void setupPersonControllerTest() {
-        hobbyInfoRepository.deleteAll();
-        personRepository.deleteAll();
-        personRepository.save(new Person("test1@email.dk", "Anders", "Andersen", 88888888));
-        personRepository.save(new Person("test2@email.dk", "Bent", "Bendsen", 88888888));
-        personRepository.save(new Person("test3@email.dk", "Charlotte", "Caroline", 88888888));
+        // hobbyInfoRepository.deleteAll();
     }
 
+    @Test
+    void testAddPerson() throws Exception {
+        PersonRequest personRequest = new PersonRequest("first","last",88888888,null,"testUser","test@mail.dk","s3cr3etpass");
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/persons")
+                        .contentType("application/json")
+                        .accept("application/json")
+                        .content(objectMapper.writeValueAsString(personRequest)))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+        // Verify that it actually ended in the database
+        assertEquals(1, personRepository.count());
+    }
 }
